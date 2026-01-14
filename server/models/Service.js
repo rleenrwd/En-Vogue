6b88d9dc8a2afb6a2d5a877cd1feeb53106121d2
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-01-14 05:14:36 -0800

    Created the models folder and the service model file. Defined service model schema.

---
+++ server/models/Service.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const serviceSchema = new Schema(
+    {
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    image: {type: String, required: true},
+    price: {type: Number, required: true},
+    durationMins: {type: Number, required: true},
+
+    },
+    {
+    timestamps: true
+    }
+);
+
+
+module.exports = new mongoose.model('Service', serviceSchema);
