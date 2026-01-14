964823e78d90e7ff750cc0e7c8041083be6c4a46
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-01-14 01:29:42 -0800

    Created the index file and got express server running successfully.

---
+++ server/index.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log('App now listening on port:', PORT)
+})
