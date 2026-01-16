ce9cb1593a4d7d92581055822707f6b27936cea6
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-01-16 01:12:20 -0800

    Created logic for the logout controller func. Added the controller funcs to the auth routes. Wired the auth routes inside index.js.

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const connectDb = require('./database/db');
 const serviceRoutes = require('./routes/serviceRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 connectDb();
 
@@ -21,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/services', serviceRoutes);
-
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
