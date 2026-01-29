2f17946c098305aee6da37487fded089ab961891
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-01-28 23:58:01 -0800

    Removed the filter Joi schema from the booking validator file. No longer implementing filtering in V1. Sticking to decisions made for V1.

--- server/validators/bookingValidator.js
+++ server/validators/bookingValidator.js
@@ -66,12 +66,5 @@ const availabilityQuerySchema = Joi.object({
 });
 
 
-const listBookingQuerySchema = Joi.object({
-    date: Joi.string()
-    .pattern(/^\d{4}-\d{2}-\d{2}$/)
-    .messages({
-        'string.pattern.base': 'Date must be in YYYY-MM-DD format.',
-    }),
-})
 
-module.exports = {createBookingSchema, availabilityQuerySchema, listBookingQuerySchema};
+module.exports = {createBookingSchema, availabilityQuerySchema};
