54e5aaa07f64a2a539a9a3cb0b8c8c1f76f69558
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-02-04 20:25:47 -0800

    Created the public createMessage controller func.

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -2,3 +2,41 @@ const mongoose = require('mongoose');
 const Message = require('../models/Message');
 const {createMessageSchema} = require('../validators/messageValidator');
 
+exports.createMessage = async (req, res) => {
+    const {error, value} = createMessageSchema.validate(req.body, {
+        abortEarly: true,
+        stripUnknown: true
+    });
+
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            message: error.details[0].message
+        });
+    }
+
+
+    const {name, phone, email, message} = value;
+
+    try {
+        const newMessage = await Message.create({
+            name,
+            phone,
+            email,
+            message
+        });
+
+        return res.status(201).json({
+            success: true,
+            data: newMessage
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error.'
+        });
+    }
+}
+
+
