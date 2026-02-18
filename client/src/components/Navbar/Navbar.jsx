da47bb27ea08f457f0c757d147f5d866ac637c9a
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-02-17 19:22:02 -0800

    Added more styles to the navbar. Nearing v1 completion.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -10,116 +10,115 @@ export default function Navbar() {
         <>
             <PageBanner />
 
-            <div className="container-fluid px-0">
                 <nav className="navbar navbar-expand-md main-navbar sticky-top">
-                    <div className="container-fluid px-0 px-md-4">
-
                     {/* MOBILE LAYOUT */}
                     {isMobile ? (
                         <>
-                            <div className="container-fluid text-center">
-                                <div className="row pt-2 align-items-center">
-                                    <div className="col-3">
-                                        <a href="#" className='nav-envelope'>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
-                                                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
-                                            </svg>
-                                        </a>
-                                    </div>
-                                    <div className="col-6">
-                                        <a className="navbar-brand d-block mx-auto m-0 p-0" href="#">
-                                            <img 
-                                            src={logo} 
-                                            alt="En Vogue Grooming Logo"
-                                            id="brand-logo"
-                                            />
-                                        </a>
-                                    </div>
+                            <div className="container-fluid px-0 px-md-4">
+                                <div className="container-fluid text-center">
+                                    <div className="row pt-2 align-items-center">
+                                        <div className="col-3">
+                                            <a href="#" className='nav-envelope'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
+                                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                                                </svg>
+                                            </a>
+                                        </div>
+                                        <div className="col-6">
+                                            <a className="navbar-brand d-block mx-auto m-0 p-0" href="#">
+                                                <img 
+                                                src={logo} 
+                                                alt="En Vogue Grooming Logo"
+                                                id="brand-logo"
+                                                />
+                                            </a>
+                                        </div>
 
-                                    <div className="col-3">
-                                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                                            <span className="navbar-toggler-icon"></span>
-                                        </button>
+                                        <div className="col-3">
+                                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                                <span className="navbar-toggler-icon"></span>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                                <div className="navbar-nav me-auto">
-                                    <a className='nav-link nav-link-container d-flex justify-content-between align-items-center nav-first-child' href="#">
-                                        <span>Home</span>
-                                        <span className="material-symbols-outlined pe-3">
-                                            chevron_right
-                                        </span>
-                                    </a>
+                                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                    <div className="navbar-nav me-auto">
+                                        <a className='nav-link nav-link-container d-flex justify-content-between align-items-center nav-first-child' href="#">
+                                            <span>Home</span>
+                                            <span className="material-symbols-outlined pe-3">
+                                                chevron_right
+                                            </span>
+                                        </a>
 
-                                    <a className='nav-link nav-link-container d-flex justify-content-between align-items-center ' href="#">
-                                        <span>About</span>
-                                        <span className="material-symbols-outlined pe-3">
-                                            chevron_right
-                                        </span>
-                                    </a>
+                                        <a className='nav-link nav-link-container d-flex justify-content-between align-items-center ' href="#">
+                                            <span>About</span>
+                                            <span className="material-symbols-outlined pe-3">
+                                                chevron_right
+                                            </span>
+                                        </a>
 
-                                    <a className='nav-link nav-link-container d-flex justify-content-between align-items-center' href="#">
-                                        <span>Services</span>
-                                        <span className="material-symbols-outlined pe-3">
-                                            chevron_right
-                                        </span>
-                                    </a>
+                                        <a className='nav-link nav-link-container d-flex justify-content-between align-items-center' href="#">
+                                            <span>Services</span>
+                                            <span className="material-symbols-outlined pe-3">
+                                                chevron_right
+                                            </span>
+                                        </a>
 
-                                    <a className='nav-link nav-link-container d-flex justify-content-between align-items-center' href="#">
-                                        <span>Testimonials</span>
-                                        <span className="material-symbols-outlined pe-3">
-                                            chevron_right
-                                        </span>
-                                    </a>
+                                        <a className='nav-link nav-link-container d-flex justify-content-between align-items-center' href="#">
+                                            <span>Testimonials</span>
+                                            <span className="material-symbols-outlined pe-3">
+                                                chevron_right
+                                            </span>
+                                        </a>
 
-                                    <a className='nav-link nav-link-container d-flex justify-content-between align-items-center nav-last-child' href="#">
-                                        <span>Contact</span>
-                                        <span className="material-symbols-outlined pe-3">
-                                            chevron_right
-                                        </span>
-                                    </a>
-                                </div>
+                                        <a className='nav-link nav-link-container d-flex justify-content-between align-items-center nav-last-child' href="#">
+                                            <span>Contact</span>
+                                            <span className="material-symbols-outlined pe-3">
+                                                chevron_right
+                                            </span>
+                                        </a>
+                                    </div>
 
-                                <div className="nav-btn-container">
-                                    <button className="nav-booknow-btn">Book Now</button>
+                                    <div className="nav-btn-container">
+                                        <button className="nav-booknow-btn">Book Now</button>
+                                    </div>
                                 </div>
                             </div>
                         </>
                     ) : (
                         <>
-                            {/* TABLET & DESKTOP LAYOUT */}
-                            <a className="navbar-brand" href="#">
-                                <img 
-                                src={logo} 
-                                alt="En Vogue Grooming Logo"
-                                id="brand-logo"
-                                />
-                            </a>
+                            <div className="container px-0 px-md-4">
+                                {/* TABLET & DESKTOP LAYOUT */}
+                                <a className="navbar-brand" href="#">
+                                    <img 
+                                    src={logo} 
+                                    alt="En Vogue Grooming Logo"
+                                    id="brand-logo"
+                                    />
+                                </a>
 
-                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
+                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span className="navbar-toggler-icon"></span>
+                                </button>
 
-                            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                                <div className="navbar-nav me-auto gap-3">
-                                    <a className="nav-link" aria-current="page" href="#">Home</a>
-                                    <a className="nav-link" href="#">About </a>
-                                    <a className="nav-link" href="#">Services</a>
-                                    <a className="nav-link" href="#">Testimonials</a>
-                                    <a className="nav-link" href="#">Contact</a>
-                                </div>
-                                <div className="nav-btn-container">
-                                    <button className="nav-booknow-btn">Book Now</button>
+                                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                    <div className="navbar-nav me-auto gap-3">
+                                        <a className="nav-link" aria-current="page" href="#">Home</a>
+                                        <a className="nav-link" href="#">About </a>
+                                        <a className="nav-link" href="#">Services</a>
+                                        <a className="nav-link" href="#">Testimonials</a>
+                                        <a className="nav-link" href="#">Contact</a>
+                                    </div>
+                                    <div className="nav-btn-container">
+                                        <button className="nav-booknow-btn">Book Now</button>
+                                    </div>
                                 </div>
                             </div>
                         </>
                     )}
-                    </div>
                 </nav>
-            </div>
-        </>
+            </>
 
     )
 }
