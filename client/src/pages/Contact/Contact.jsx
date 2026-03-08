341e3c987586287918c67e6d9a94168df7a50b89
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-03-08 04:05:33 -0700

    Deleted contact page content. Will start fresh tomorrow.

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,18 +1,9 @@
+
+import "./Contact.css";
+
 export default function Contact() {
     return (
         <>
-            <section className="contact-container">
-                <div className="card mb-3">
-                    <div className="row">
-                        <div className="col-md-6">
-                            
-                        </div>
-                        <div className="col-md-6">
-                            <h1 className="display-4 d-md-none">Contact page</h1>
-                        </div>
-                    </div>
-                </div>
-            </section>
             
         </>
     )
