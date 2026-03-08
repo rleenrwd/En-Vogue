a4a764c03ddef317a52719c294de50acc1ed79bc
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-03-08 03:16:33 -0700

    Created the structure of the contact page.

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,3 +1,20 @@
 export default function Contact() {
-    return <h1>Contact page</h1>
+    return (
+        <>
+            <section className="contact-container">
+                <div className="card mb-3">
+                    <div className="row">
+                        <div className="col-md-6">
+                            
+                        </div>
+                        <div className="col-md-6">
+                            <h1 className="display-4 d-md-none">Contact page</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+        </>
+    )
+    
 }
