e66cf325a942ae9b92a9dab964de5ac8612da00c
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-03-13 23:17:23 -0700

    Created a template login form. Created the associated css file.

--- client/src/admin/pages/Login/Login.jsx
+++ client/src/admin/pages/Login/Login.jsx
@@ -1,8 +1,40 @@
-
+import {useState} from 'react';
+import "./Login.css";
 
 export default function Login() {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+
     return (
-        <h1>Login page</h1>
+        <>
+            <section className="login-form">
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </section>
+        </>
     )
 }
 
